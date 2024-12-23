c6401c4aca7db18b3ba85f47d0dcc9e1235fe4fc
Ayushmaanagarwal1121 <agarwalayushmaan88@gmail.com>
2024-12-23 19:21:07 +0530

    updated

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from 'react'
 
 export default function Timer({gameOver}) {
-  const [time,setTime] = useState(0)
-      if(time >= 11){
+  const [time,setTime] = useState(10)
+      if(time ==0){
         gameOver()
         return 
       }
-      setTimeout(()=>setTime(time+1),1000)
+      setTimeout(()=>setTime(time-1),1000)
 
   return (
     <div>{time}</div>
